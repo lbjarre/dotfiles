fae80681fd11443eb665e1a9c5d7f2b2ae61579d
Lukas Bjarre <lukas.bjarre@northvolt.com>
2023-03-02 21:21:05 +0100

    Remove unused globals

--- nvim/lua/skr/init.lua
+++ nvim/lua/skr/init.lua
@@ -33,21 +33,3 @@ local pkgs = require('skr.pkg')
 require('lazy').setup(pkgs)
 -- Options
 require('skr.options').setup()
-
--- P is a print-inspect helper, prints the expanded table and returns the
--- argument back to the caller.
-function P(x)
-	print(vim.inspect(x))
-	return x
-end
-
--- R forcefully reloads a lua module.
-function R(name)
-	-- default to this init module if name is missing.
-	if not name then
-		name = 'skr'
-	end
-
-	require('plenary.reload').reload_module(name)
-	return require(name)
-end
