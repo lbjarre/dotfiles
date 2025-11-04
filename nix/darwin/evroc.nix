88cba6405ba0dbb4af0bd3fd46bac1275570b5ea
Lukas Bjarre <lukas.bjarre@gmail.com>
2025-11-04 17:35:37 +0100

    darwin: Manage more system.defaults in nix-darwin

--- nix/darwin/evroc.nix
+++ nix/darwin/evroc.nix
@@ -54,6 +54,37 @@ in
   system.stateVersion = 6;
   system.primaryUser = name;
 
+  system.defaults = {
+    # Autohide the dock, more screen real estate.
+    dock.autohide = true;
+
+    dock.persistent-apps = [
+      { app = "/Applications/Firefox.app"; }
+      { app = "/Applications/Nix Apps/WezTerm.app"; }
+      { app = "/Applications/Nix Apps/AeroSpace.app"; }
+      { app = "/Applications/Mattermost.app"; }
+      { app = "/Applications/Webex.app"; }
+      { app = "/Applications/Spotify.app"; }
+      { app = "/System/Applications/Calendar.app"; }
+      { app = "/System/Applications/Mail.app"; }
+    ];
+
+    # Turn off "natural" scroll direction.
+    NSGlobalDomain."com.apple.swipescrolldirection" = false;
+
+    finder = {
+      AppleShowAllExtensions = true;
+      ShowPathbar = true;
+      FXEnableExtensionChangeWarning = false;
+    };
+
+    # Show more controls in menu bar by default.
+    controlcenter = {
+      Bluetooth = true;
+      Sound = true;
+    };
+  };
+
   # Enable sudo with TouchID.
   security.pam.services.sudo_local = {
     enable = true;
